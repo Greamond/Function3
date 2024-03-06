f0911f44065cf6628f4bc75a9c9c42fd3437b7f9
student-a208 <student-a208@PC-A208-9>
2024-03-06 14:18:06 +0500

    Классы Doctor и MedicalCard

---
+++ Function3/Models/Doctor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Function3.Models
+{
+    public class Doctor
+    {
+        /// <summary>
+        /// Код врача
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Логин врача
+        /// </summary>
+        public string Login { get; set; }
+        /// <summary>
+        /// Пароль врача
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Должность врача
+        /// </summary>
+        public string Position { get; set; }
+    }
+}
---
+++ Function3/Models/MedicalCard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Function3.Models
+{
+    public class MedicalCard
+    {
+        /// <summary>
+        /// Код мед.карты
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Код пациента
+        /// </summary>
+        public int IdPatient { get; set; }
+        /// <summary>
+        /// Информация о пциенте
+        /// </summary>
+        public string Inforamtion { get; set; }
+        /// <summary>
+        /// Симптомы
+        /// </summary>
+        public string Symptoms { get; set; }
+        /// <summary>
+        /// Диагноз 
+        /// </summary>
+        public string Diagnosis { get; set; }
+        /// <summary>
+        /// Рекомендации по лечению
+        /// </summary>
+        public string TreatmentRecom { get; set; }
+        /// <summary>
+        /// Рецепты
+        /// </summary>
+        public string Recipes { get; set; }
+        /// <summary>
+        /// НАправления к специалистам
+        /// </summary>
+        public string DirectionSpecialist { get; set; }
+        /// <summary>
+        /// Иисследования
+        /// </summary>
+        public string Research { get; set; }
+        /// <summary>
+        /// Процедуры
+        /// </summary>
+        public string Procedures { get; set; }
+    }
+}
